eefb392015c9ba0f4243a56b45e6a650950dfbbd
sebastianbernalc <sebastianbernalc@udea.edu.co>
2024-07-25 19:18:49 -0500

    Commit test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Proceso import process
-'''This is the main script that will run the process function from the process.py module.  '''
+'''This is the main script that will run the process function from the process.py module.'''
 if __name__ == '__main__':
     df = process.proceso()
+
